9e2bc337704ffe667cb3920bd0ec2d5140669d88
Dhruv Govani <dhruv.govani@bkn301.com>
2026-06-18 13:15:05 +0530

    Update UI binary checksums for PlatformKit-aware builds.

--- Package.swift
+++ Package.swift
@@ -18,17 +18,17 @@ let package = Package(
         .binaryTarget(
             name: "DesignKit",
             url: "\(baseURL)/DesignKit.xcframework.zip",
-            checksum: "1f2858d99898406047b3dd243747e8233c2e77a5fce3743b6eae360e86b44482"
+            checksum: "2ac159fa28c9bf026d65f3c41b3f0733ff6648c6ed7ed9811456ebed6669e7fd"
         ),
         .binaryTarget(
             name: "AccountsUI",
             url: "\(baseURL)/AccountsUI.xcframework.zip",
-            checksum: "bf0bf628201cb7129cedef8fd303680921ce4b544b4465e479ec9f31a2752948"
+            checksum: "91c20b8b36d6506e68dd00fdc690e640059ccf83b445aca14d0cea2ccac7e406"
         ),
         .binaryTarget(
             name: "CardsUI",
             url: "\(baseURL)/CardsUI.xcframework.zip",
-            checksum: "f8d4dd972af70b6b964730a3b306125566da736596d6a2b9e0f2fc2c19a6dbb4"
+            checksum: "698d3d76b6b64a82185172b5c7e99dc4b9d9b8e783cac92ed477c804e22ec25a"
         ),
     ]
 )
