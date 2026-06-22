798724cb479e13b97ba4341752d50427aac4b796
Dhruv Govani <dhruv.govani@bkn301.com>
2026-06-22 19:50:25 +0530

    Fix checksums for 0.1.12-SNAPSHOT

--- Package.swift
+++ Package.swift
@@ -16,17 +16,17 @@ let package = Package(
         .binaryTarget(
             name: "DesignKit",
             url: "\(baseURL)/DesignKit.xcframework.zip",
-            checksum: "2548f317d8336c914c3fd417daf61f3270a657e38fa9f6b09dc107daaafcdff8"
+            checksum: "a4f64427f419d662744d659f0258f868c6afee5c2e4fd5213c4634d11f6f1366"
         ),
         .binaryTarget(
             name: "AccountsUI",
             url: "\(baseURL)/AccountsUI.xcframework.zip",
-            checksum: "0d6b4e30ec3acc8958cf742c6e079c288ee6b39f50585f9da0ec159f87d137cd"
+            checksum: "983fb4fbc902d7a7bf06b15243f012e7f6819e390c9ed98a3cb984953b01b510"
         ),
         .binaryTarget(
             name: "CardsUI",
             url: "\(baseURL)/CardsUI.xcframework.zip",
-            checksum: "a080a55ab2b95229deaa4f97c6d629a2b896795e9faadb7941de555caad8e99f"
+            checksum: "2548f317d8336c914c3fd417daf61f3270a657e38fa9f6b09dc107daaafcdff8"
         ),
     ]
 )
